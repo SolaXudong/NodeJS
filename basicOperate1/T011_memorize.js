7b429e43ebe97186ad0cc364031fabb3da008b96
SolaXudong <zgc_xudong@163.com>
2020-05-08 17:14:30 +0800

    #

--- basicOperate1/T011_memorize.js
+++ basicOperate1/T011_memorize.js
@@ -1,9 +1,7 @@
 for (var i = 1; i <= 5; i++) {
     var str = '';
     for (var j = 1; j <= 5; j++) {
-        let f = parseFloat(Math.random(9) + 1).toFixed(0);
-        let l = parseFloat(Math.random(10)).toFixed(0);
-        str += f + "" + l;
+        str += parseInt(Math.random() * 9 + 1, 10) + '' + parseInt(Math.random() * 10, 10);
         if (j != 5)
             str += '\t';
     }
