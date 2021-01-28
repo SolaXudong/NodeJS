9b1bd420e617d4492c69d008ab9e00c255ff9517
SolaXudong <zgc_xudong@163.com>
2021-01-28 13:43:34 +0800

    #

--- basicOperate1/T013_MySQL.js
+++ basicOperate1/T013_MySQL.js
@@ -7,10 +7,10 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 let _cost = new Date().getTime();
-let _total = 100000; // 1秒10万
+let _total = 10_0000; // 查询：1秒10万，添加：100万10秒
 /** 单个查询 */
 // for (let index = 1; index <= _total; index++) {
-//     var sql = `select * from user2 where id = ${index}`;
+//     var sql = `select * from user_new where id = ${index}`;
 //     connection.query(sql, (error, results, fields) => {
 //         if (error) throw error;
 //         // console.log('result: ', JSON.parse(JSON.stringify(results[0])));
@@ -19,7 +19,7 @@ let _total = 100000; // 1秒10万
 //     });
 // }
 /** 批量查询 */
-// var sql = `select * from user2 where id in ( `;
+// var sql = `select * from user_new where id in ( `;
 // for (let index = 1; index <= _total; index++) {
 //     if (index == 1)
 //         sql += `${index}`;
@@ -34,22 +34,21 @@ let _total = 100000; // 1秒10万
 // });
 /** 单个添加 */
 // for (let index = 1; index <= _total; index++) {
-//     // var sql = `SELECT * FROM user2 limit 3`;
-//     var sql = `insert into user2() values(null, '徐${index}', ${index}, sysdate())`;
+//     var sql = `insert into user_new() values(null, '哈哈_${index}', ${index + 10}, now())`;
 //     connection.query(sql, (error, results, fields) => {
 //         if (error) throw error;
 //         // console.log('result: ', results);
-//         if (index % 10000 == 0 || index == _total)
+//         if (index % 1000 == 0 || index == _total)
 //             console.log('##### over ', index, '/', _total, 'cost : ', (new Date().getTime() - _cost) / 1000, 's');
 //     });
 // }
 /** 批量添加 */
-// var sql = `insert into user2() values`;
+// var sql = `insert into user_new() values`;
 // for (let index = 1; index <= _total; index++) {
 //     if (index == 1)
-//         sql += `(null, '徐${index}', ${index}, now())`;
+//         sql += `(null, '哈哈_${index}', ${index + 10}, sysdate())`;
 //     else
-//         sql += `,(null, '徐${index}', ${index}, now())`;
+//         sql += `,(null, '哈哈_${index}', ${index + 10}, sysdate())`;
 // }
 // connection.query(sql, (error, results, fields) => {
 //     if (error) throw error;
--- basicOperate1/T014_Mongo.js
+++ basicOperate1/T014_Mongo.js
@@ -1,10 +1,10 @@
 // 代码参考：https://www.cnblogs.com/zp900704/p/11688041.html
 const mongoclient = require("mongodb");
 mongoclient.connect("mongodb://debt_test:o4GtROXZF9tXdzJq@47.110.135.139:9717/debt_test_new", { useUnifiedTopology: true }, function (err, client) {
-    let _total = 10000; // 1万6秒
+    let _total = 1000; // 1万6秒
     let _cost = new Date().getTime();
     for (let index = 1; index <= _total; index++) {
-        client.db("debt_test_new").collection("user").updateMany({ name: "徐_1" }, { $set: { age: index, name: "徐_1" } }, { multi: true }).then(function (result) {
+        client.db("debt_test_new").collection("user").updateMany({ name: "徐_1" }, { $set: { age: 2000_0000 + index, name: "徐_1" } }, { multi: true }).then(function (result) {
             // console.log(result.result);
             if (index == _total) {
                 console.log('##### over cost : ', (new Date().getTime() - _cost) / 1000, 's');
